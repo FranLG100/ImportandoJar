917f15336a522838554b83ba8c63b425b007fd00
Fran <fralg100@gmail.com>
2019-12-03 18:56:41 +0100

    Anhadido nuevo jar

--- src/pruebasjar/PruebasJAR.java
+++ src/pruebasjar/PruebasJAR.java
@@ -19,12 +19,33 @@ public class PruebasJAR {
      */
     public static void main(String[] args) {
         DAOFactory factory=new SqlDbDAOFactory();
+        //DAOFactory factory=new NeodatisDAOFactory();
+        
         DepartamentoDAO dpto=factory.getDepartamentoDAO();
         EmpleadoDAO empleado=factory.getEmpleadoDAO();
-        Empleado auxEm=new Empleado(5, "Sanchez", 1, 1, LocalDate.of(2019, 1, 19), "Becario", 350.0);
+        
         Departamento auxDpto=new Departamento(2, "Contabilidad", "Madrid");
         
+        Empleado auxEm=new Empleado(5, "Sanchez", 2, 1, LocalDate.of(2019, 1, 19), "Becario", 350.0);
+        
+        
+        //Insertamos Departamento
+        
+        //Modificamos Departamento
+        
+        //Eliminamos Departamento
+        
+        
+        
+        //Insertamos empleado
         empleado.InsertarEmpleado(auxEm);
+        
+        //Modificamos Empleado
+        
+        //Eliminamos Empleado
+        
+        
+        
         
     }
     
